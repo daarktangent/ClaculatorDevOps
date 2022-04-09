02e31a33ccac8b04a721bdeb9903e250cf269556
darktangent <aman.jha0409@gmail.com>
2022-04-09 16:17:01 +0530

    Initial commit

---
+++ src/main/java/com/iiitb/claculatordevops/ClaculatorDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.claculatordevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaculatorDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaculatorDevOpsApplication.class, args);
+	}
+
+}
